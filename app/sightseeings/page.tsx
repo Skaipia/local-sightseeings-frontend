398550cf39f5caf18ec1dceadc85066458a71e72
Daryyya <dara9841402670@gmail.com>
2026-05-06 22:31:43 +1000

    fix styles

--- app/sightseeings/page.tsx
+++ app/sightseeings/page.tsx
@@ -1,11 +1,103 @@
 'use client';
 
-export default function SightseeingsPage() {
-    return (
-        <div className="min-h-screen bg-gray-50">
-            <main className="container mx-auto px-4 py-8">
-            </main>
+import { SightCard } from '@/src/components/sight-card';
+import s from './styles.module.css';
+import cn from 'classnames';
+import { useState } from 'react';
+import { CatalogFilter, FilterItem } from '@/src/components/catalog-filter';
+import { SearchInput } from '@/src/components/search';
+import { SortBar, SortOption } from '@/src/components/sort-bar';
+
+const items: FilterItem[] = [
+  { id: 1, label: 'Apple' },
+  { id: 2, label: 'Samsung' },
+  { id: 3, label: 'Xiaomi' },
+  { id: 4, label: 'Huawei' },
+  { id: 5, label: 'Honor' },
+  { id: 6, label: 'Realme' },
+];
+
+const options: SortOption[] = [
+  { value: 'popular', label: 'Популярности' },
+  { value: 'rating', label: 'Рейтингу' },
+];
+
+export default function Home() {
+  const [selected, setSelected] = useState<(string | number)[]>([]);
+  const [search, setSearch] = useState('');
+  const [sort, setSort] = useState('popular');
+
+  return (
+    <div className="container mx-auto px-8 pt-8 max-w-[1440px]">
+      <h1 className={cn('font-golos', s.title)}>Достопримечательности</h1>
+      <div className={s.page}>
+        <aside className={s.aside}>
+          <CatalogFilter title="Бренд" items={items} selected={selected} onChange={setSelected} />
+          <CatalogFilter title="Бренд" items={items} selected={selected} onChange={setSelected} />
+          <CatalogFilter title="Бренд" items={items} selected={selected} onChange={setSelected} />
+          <CatalogFilter title="Бренд" items={items} selected={selected} onChange={setSelected} />
+          <CatalogFilter title="Бренд" items={items} selected={selected} onChange={setSelected} />
+          <div className={s.buttons}>
+            <button className={s.submit}>Применить</button>
+            <button className={s.reset}>Сбросить</button>
+          </div>
+        </aside>
+        <div className={s.main}>
+          <SearchInput value={search} onChange={setSearch} />
+          <SortBar options={options} value={sort} onChange={setSort} />
+
+          <div className={s.content}>
+            <SightCard
+              imageUrl="/test-sight-card.png"
+              sightName="Памятник Преданности"
+              sightShortDescription="Пес Верный ждал хозяина 7 лет на остановке"
+              sightLocation="Тольятти"
+            />
+            <SightCard
+              imageUrl="/test-sight-card.png"
+              sightName="Памятник Преданности"
+              sightShortDescription="Пес Верный ждал хозяина 7 лет на остановке"
+              sightLocation="Тольятти"
+            />
+            <SightCard
+              imageUrl="/test-sight-card.png"
+              sightName="Памятник Преданности"
+              sightShortDescription="Пес Верный ждал хозяина 7 лет на остановке"
+              sightLocation="Тольятти"
+            />
+            <SightCard
+              imageUrl="/test-sight-card.png"
+              sightName="Памятник Преданности"
+              sightShortDescription="Пес Верный ждал хозяина 7 лет на остановке"
+              sightLocation="Тольятти"
+            />
+            <SightCard
+              imageUrl="/test-sight-card.png"
+              sightName="Памятник Преданности"
+              sightShortDescription="Пес Верный ждал хозяина 7 лет на остановке"
+              sightLocation="Тольятти"
+            />
+            <SightCard
+              imageUrl="/test-sight-card.png"
+              sightName="Памятник Преданности"
+              sightShortDescription="Пес Верный ждал хозяина 7 лет на остановке"
+              sightLocation="Тольятти"
+            />
+            <SightCard
+              imageUrl="/test-sight-card.png"
+              sightName="Памятник Преданности"
+              sightShortDescription="Пес Верный ждал хозяина 7 лет на остановке"
+              sightLocation="Тольятти"
+            />
+            <SightCard
+              imageUrl="/test-sight-card.png"
+              sightName="Памятник Преданности"
+              sightShortDescription="Пес Верный ждал хозяина 7 лет на остановке"
+              sightLocation="Тольятти"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-       
